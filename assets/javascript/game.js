9ec2a232fb30d8c5e8236a1c8babbf8d06156239
Eugene Lee <eug703@gmail.com>
2019-09-25 01:37:24 -0400

    logic is completed, will refine, add css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,74 @@
 var random_result;
-var lost = "";
-var win = "";
+var lost = 0;
+var win = 0;
+var previous = 0;
 
 
-// random total you're supposed to guess is generated here then snet to result p tag in html
-random_result = Math.floor(Math.random() * 101) + 19;
-console.log(random_result);
+var resetAndStart = function() {
 
-$("#result").html("Random Result: " + random_result);
-// for loop. the number here determines the number of crystal divs generated.
-for(var i = 0; i < 4; i++){
-   
-    var random = Math.floor(Math.random() * 12);
-    console.log(random);
-   
-    var crystal = $("<div>");
-        crystal.attr({
-            "class": 'crystal',
-            "data-random": random
-        });
+            // empties the existing crystal divs so a new set can generate
+            $(".crystals").empty();
 
-     $(".crystals").append(crystal);
+            // random total you're supposed to guess is generated here then snet to result p tag in html
+            random_result = Math.floor(Math.random() * 101) + 19;
+            console.log(random_result);
+
+            $("#result").html("Random Result: " + random_result);
+
+
+            // for loop. the number here determines the number of crystal divs generated.
+            for(var i = 0; i < 4; i++){
+            
+                var random = Math.floor(Math.random() * 11) + 1;
+                console.log(random);
+                // the random function in var random is written as *11)+1 to exclude generating zeroes
+            
+                var crystal = $("<div>");
+                    crystal.attr({
+                        "class": 'crystal',
+                        "data-random": random
+                    });
+
+                $(".crystals").append(crystal);
+            }
+            // end of for loop
 }
+
+
+// start of the guts
+// runs the game function
+resetAndStart();
+
+// This is called event delegation, read more into it and why its important for later
+$(document).on("click", ".crystal", function() {
+
+
+
+// $(".crystal").on("click", function () {
+
+
+
+    var num = parseInt($(this).attr("data-random"));
+// console.log(num);
+    // calls the data-random attribute of the var crystal which contains the randomly generated number for the gem
+    previous += num;
+console.log(previous);
+
+    if(previous > random_result){
+        lost--;
+        console.log("You Lost");
+        $("#lost").html("Losses: " + lost);
+        previous = 0;
+        resetAndStart();
+    }
+
+    else if( previous === random_result){
+        win++;
+        console.log("You Win");
+        $("#win").html("Wins: " + win);
+        previous = 0;
+        resetAndStart();
+    }
+
+});
+
